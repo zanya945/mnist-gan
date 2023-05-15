1fdb706b4af9ccba3c1f35c1e5cac88fbbf16a44
zanya945 <tea915656@gmail.com>
2023-05-15 22:20:55 +0800

    finsh cnn

--- Model.py
+++ Model.py
@@ -1,6 +1,6 @@
 import tensorflow as tf
 from keras.models import Sequential
-from keras.layers import Conv2D, MaxPool2D, Flatten, Dense
+from keras.layers import Conv2D, MaxPool2D, Flatten, Dense, Dropout
 
 def layer(input, wshape, bshape):
     w = tf.compat.v1.get_variable('w', wshape, initializer=tf.random_normal_initializer(stddev=(2.0 / wshape[0]) ** 0.5))
@@ -22,19 +22,15 @@ class Model:
         with tf.compat.v1.variable_scope('output'):
             yhat = layer(Y2, [n2, n3], n3)
         return yhat
-    def cnn(x, input, output):
-        model = Sequential()
-        model = model.add(Conv2D(32, 5, 1, input_shape=(28, 28, 1), activation=tf.nn.relu))
-        model = model.add(MaxPool2D(pool_size=(2,2), strides=2))
-        model = model.add(Conv2D(32, 5, 1, activation=tf.nn.relu))
-        model = model.add(MaxPool2D(pool_size=(2,2)))
-        model = model.add(Conv2D(32, 5, 1, activation=tf.nn.relu))
-        model = model.add(MaxPool2D(pool_size=(2,2)))
-        model = model.add(Flatten())
-        model = model.add(Dense(256, activation= tf.nn.relu))
-        model = model.add(Dense(83, activation= tf.nn.relu))
-        model = model.add(Dense(10, activation= tf.nn.softmax))
-        model.build()
-        model.summary()
+    def cnn(model):
+        model.add(Conv2D(filters=6, kernel_size=(5, 5), strides=1, padding='same', input_shape=(28, 28, 1), activation=tf.nn.relu))
+        model.add(MaxPool2D(pool_size=(2,2), strides=2))
+        model.add(Conv2D(filters=16, kernel_size=(5, 5), strides=1, padding='valid', activation=tf.nn.relu))
+        model.add(MaxPool2D(pool_size=(2,2), strides=2))
+        model.add(Conv2D(filters=120, kernel_size=(5,5), strides=1, padding='valid', activation=tf.nn.relu))
+        model.add(Dropout(0))
+        model.add(Flatten())
+        model.add(Dense(84, activation= tf.nn.relu))
+        model.add(Dense(10, activation= tf.nn.softmax))
         return model
 
--- main.py
+++ main.py
@@ -3,42 +3,59 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 import Preprocessing
+from keras.models import Sequential, load_model
 from Model import Model
 from Preprocessing import Proprecessing
 
 
-xtrain, ytrain, x_test, y_test = Preprocessing.Proprecessing.load_data()
-lr = 0.1
-batchsize = 128
-epoch = 4000
+xtrain, ytrain, x_test, y_test = Preprocessing.Proprecessing.load_data_cnn()
+# lr = 0.1
+# batchsize1 = 128
+# epoch1 = 10
+#
+# model = Sequential()
+# Model.cnn(model)
+# model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+# model.fit(xtrain, ytrain, epochs=epoch1, batch_size=batchsize1, verbose=1)
+#
+# loss, acc = model.evaluate(x_test, y_test)
+# print(loss, acc)
+# model.save('./CNN_Mnist.h5')
+model = load_model('./CNN_Mnist.h5')
+# input = xtrain.shape[1] #784
+# output = ytrain.shape[1] #10
+# tf.compat.v1.disable_eager_execution()
+# X = tf.compat.v1.placeholder(tf.float32, shape=[None, input])
+# Y = tf.compat.v1.placeholder(tf.float32, shape=[None, output])
+#
+# yhat = Model.logistic(X, input, output)
+# pred = tf.nn.softmax(yhat)
+# lossop = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=yhat, labels=Y))
+# optimizer = tf.compat.v1.train.GradientDescentOptimizer(lr)
+# train_op = optimizer.minimize(lossop)
+# correct = tf.equal(tf.argmax(pred, 1), tf.argmax(Y, 1))
+# accuracy = tf.reduce_mean(tf.cast(correct, tf.float32))
+# init = tf.compat.v1.global_variables_initializer()
+#
+# with tf.compat.v1.Session() as sess:
+#     sess.run(init)
+#     for epoch in range(epoch):
+#         xbatch, ybatch = Preprocessing.Proprecessing.batch_size(batchsize, xtrain, ytrain)
+#         sess.run(train_op, feed_dict={X: xbatch, Y: ybatch})
+#         loss, acc = sess.run([lossop, accuracy], feed_dict={X: xbatch, Y: ybatch})
+#     acc = sess.run(accuracy, feed_dict={X: x_test, Y: y_test})
+#     print('test acc=' + '{:.3f}'.format(acc))
+#     sess.close()
 
-input = xtrain.shape[1] #784
-output = ytrain.shape[1] #10
-tf.compat.v1.disable_eager_execution()
-X = tf.compat.v1.placeholder(tf.float32, shape=[None, input])
-Y = tf.compat.v1.placeholder(tf.float32, shape=[None, output])
-
-yhat = Model.logistic(X, input, output)
-pred = tf.nn.softmax(yhat)
-lossop = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=yhat, labels=Y))
-optimizer = tf.compat.v1.train.GradientDescentOptimizer(lr)
-train_op = optimizer.minimize(lossop)
-correct = tf.equal(tf.argmax(pred, 1), tf.argmax(Y, 1))
-accuracy = tf.reduce_mean(tf.cast(correct, tf.float32))
-init = tf.compat.v1.global_variables_initializer()
-
-with tf.compat.v1.Session() as sess:
-    sess.run(init)
-    for epoch in range(epoch):
-        xbatch, ybatch = Preprocessing.Proprecessing.batch_size(batchsize, xtrain, ytrain)
-        sess.run(train_op, feed_dict={X: xbatch, Y: ybatch})
-        loss, acc = sess.run([lossop, accuracy], feed_dict={X: xbatch, Y: ybatch})
-    acc = sess.run(accuracy, feed_dict={X: x_test, Y: y_test})
-    print('test acc=' + '{:.3f}'.format(acc))
-    sess.close()
-
-def showimg(ndarr):
-    img1 = ndarr.copy()
-    img1.shape = (28, 28)
-    plt.imshow(img1, cmap="gray")
+def showimg(n):
+    plt.imshow(x_test[n], cmap="gray")
     plt.show()
+
+def one_img_predict(model, n):
+    predict = model.predict(x_test)
+    classes_x = np.argmax(predict, axis=1)
+    print('Prediction:', classes_x[n])
+    print('Answer:', y_test[n])
+    showimg(n)
+
+one_img_predict(model, 990)
