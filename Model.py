59d67ea995a3338cec0903d064e014218d1d3992
zanya945 <tea915656@gmail.com>
2023-05-15 09:06:48 +0800

    finish Discriminator network

--- Model.py
+++ Model.py
@@ -1,15 +1,21 @@
 import tensorflow as tf
 
+
+def layer(input, wshape, bshape):
+    w = tf.compat.v1.get_variable('w', wshape, initializer=tf.random.normal(stddev=(2.0 / wshape[0]) ** 0.5))
+    b = tf.compat.v1.get_variable('b', bshape, initializer=tf.constant_initializer(value=0))
+    y = tf.add(tf.matmul(w, b), input)
+    return y
 class Model:
     def logistic(X, input, output):
         n0 = input
         n1 = 256
-        n2 = output
-        w1 = tf.Variable(tf.random.normal([n0, n1]))
-        b1 = tf.Variable(tf.random.normal([n1]))
-        w2 = tf.Variable(tf.random.normal([n1, n2]))
-        b2 = tf.Variable(tf.random.normal([n2]))
-
-        Y1 = tf.add(tf.matmul(X, w1), b1)
-        yhat = tf.add(tf.matmul(Y1, w2), b2)
+        n2 = 256
+        n3 = output
+        with tf.compat.v1.variable_scope('L1'):
+            Y1 = layer(X, [n0, n1], [n1])
+        with tf.compat.v1.variable_scope('L2'):
+            Y2 = layer(Y1, [n1, n2], [n2])
+        with tf.compat.v1.variable_scope('output'):
+            yhat = layer(Y2, [n2,n3], n3)
         return yhat
